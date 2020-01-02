d3d37108e3bdbac7038eb0403aab122f145db5f2
KaizerJ <kaizeresp@gmail.com>
2020-01-02 21:29:15 +0000

    Implemented Presenter as Square observer

--- src/presenter/SquarePresenter.java
+++ src/presenter/SquarePresenter.java
@@ -1,12 +1,46 @@
 package presenter;
 
 import control.Observer;
+import model.Square;
+import view.Point;
+import view.SquareDisplay;
 
 public class SquarePresenter implements Observer{
+    public final static Point ScreenSize = new Point(400,400);
+    private Square square;
+    private SquareDisplay squareDisplay;
 
-    @Override
-    public void update() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public SquarePresenter(Square square, SquareDisplay squareDisplay) {
+        this.square = square;
+        this.squareDisplay = squareDisplay;
+        this.square.add(this);
+        this.squareDisplay.add(new SquareDisplay.Listener() {
+            @Override
+            public void newPosition(Point point) {
+                square.setPos(ScreenToModel(point));
+                update();
+            }
+        });
     }
     
+    
+    
+    @Override
+    public void update() {
+        squareDisplay.show(modelToScreen(square.getPos()));
+    }
+    
+    public Point modelToScreen(Point p){
+        return new Point(
+            p.getX() * ScreenSize.getX() / square.getBoundX(),
+            (square.getBoundY() - p.getY()) * ScreenSize.getY() / square.getBoundY()
+        );
+    }
+    
+    public Point ScreenToModel(Point p){
+        return new Point(
+                p.getX() * square.getBoundX() / ScreenSize.getX(),
+                (square.getBoundY() - p.getY()) * square.getBoundY() / ScreenSize.getY()
+        );
+    }
 }
