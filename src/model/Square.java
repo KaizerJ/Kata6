53b6deba24cee3c60e9a18721305563e49dd0a49
KaizerJ <kaizeresp@gmail.com>
2020-01-02 21:32:00 +0000

    Changed Position methods, added Bounds and Observer pattern

--- src/model/Square.java
+++ src/model/Square.java
@@ -1,29 +1,44 @@
 package model;
 
+import control.Observer;
+import java.util.ArrayList;
+import java.util.List;
+import view.Point;
+
 public class Square {
     private final int size;
     private int posX, posY;
     private final int boundX, boundY;
-
+    private final List<Observer> observers = new ArrayList<>();
+    
     public Square(int size, int boundX, int boundY) {
         this.size = size;
         this.boundX = boundX;
         this.boundY = boundY;
-        this.posX = 0;
-        this.posY = 0;
+        this.posX = boundX/2;
+        this.posY = boundX/2;
     }
     
+    public void setPos(Point point){
+        setPosX(point.getX());
+        setPosY(point.getY());
+        updateObservers();
+    }
     
-    public void setPosX(int posX) {
+    private void setPosX(int posX) {
         this.posX = posX > boundX - (size/2) || posX < (size/2) ? 
                 this.posX :  posX;
     }
 
-    public void setPosY(int posY) {
+    private void setPosY(int posY) {
         this.posY = posY > boundY - (size/2) || posY < (size/2) ? 
                 this.posY :  posY;
     }
 
+    public Point getPos(){
+        return new Point(getPosX(),getPosY());
+    }
+    
     public int getPosX() {
         return posX;
     }
@@ -34,5 +49,23 @@ public class Square {
 
     public int getSize() {
         return size;
+    }
+
+    public int getBoundX() {
+        return boundX;
+    }
+
+    public int getBoundY() {
+        return boundY;
+    }
+
+    public void add(Observer observer) {
+        this.observers.add(observer);
+    }
+
+    private void updateObservers() {
+        for (Observer observer : observers) {
+            observer.update();
+        }
     }
 }
